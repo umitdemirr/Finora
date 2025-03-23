0271cce3302a12075c65a37a6062dc80f98335ac
Ümit Demir <umit.demiir@outlook.com>
2025-03-23 04:04:21 +0300

    DbContext Eklendi

---
+++ DataAccess/Concrete/EntityFramework/PostgreSql/PostgreDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework;
+
+public class PostgreDbContext: DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=112263;Database=Finora");
+    }
+   
+    public DbSet<User> Users { get; set; }
+    public DbSet<OperationClaim> OperationClaims { get; set; }
+    public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    public DbSet<BankAccount> BankAccounts { get; set; }
+    public DbSet<BankAndExchange> BankAndExchanges { get; set; }
+    public DbSet<BankCard> BankCards { get; set; }
+    public DbSet<BankTransaction> BankTransactions { get; set; }
+    public DbSet<ExchangeRate> ExchangeRates { get; set; }
+    public DbSet<Inflation> Inflations { get; set; }
+    public DbSet<Portfolio> Portfolios { get; set; }
+    public DbSet<StockAccount> StockAccounts { get; set; }
+    public DbSet<StockTransaction> StockTransactions { get; set; }
+}
---
+++ DataAccess/Concrete/EntityFramework/SqlServer/SqlServerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework;
+
+public class SqlServerDbContext: DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Host=localhost;Port=5432;Username=postgres;Password=112263;Database=WalletManager");
+    }
+   
+    public DbSet<User> Users { get; set; }
+    public DbSet<OperationClaim> OperationClaims { get; set; }
+    public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+}
