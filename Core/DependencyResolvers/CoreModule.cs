7e8522b9895858043b98a9fec5005f5753a94eca
Ümit Demir <umit.demiir@outlook.com>
2025-03-28 02:19:22 +0300

    Result yapısı eklendi ve Core katmanı geliştirildi.

---
+++ Core/DependencyResolvers/CoreModule.cs
@@ -0,0 +1,17 @@
+﻿using Core.CrossCuttingConcerns.Caching;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
+using Core.Utilities.IoC;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.DependencyResolvers;
+
+public class CoreModule : ICoreModule
+{
+    public void Load(IServiceCollection serviceColleciton)
+    {
+        serviceColleciton.AddMemoryCache();
+        serviceColleciton.AddSingleton<IHttpContextAccessor,HttpContextAccessor>();
+        serviceColleciton.AddSingleton<ICacheManager,MemoryCacheManager>();
+    }
+}
